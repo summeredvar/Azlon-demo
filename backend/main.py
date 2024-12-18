8c0c08b705f39e843a73703a1e5593e567cc4a18
summeredvar <26641708+summeredvar@users.noreply.github.com>
2024-12-18 00:04:44 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 class UserInput(BaseModel):
